483b036b6699957530a24a20d68471086c917012
kihyun940 <houng027@gmail.com>
2024-02-07 19:31:57 +0900

    Feat: 영속성 컨텍스트 개념 파악 - 엔티티를 영구 저장하는 환경 - EntityManager.persist(entity); - 엔티티 매니저를 통해서 영속성 컨텍스트에 접근
    엔티티의 생명주기
    - 비영속 : 영속성 컨텍스트와 전혀 관계가 없는 새로운 상태
    - 영속 : 영속성 컨텍스트에 관리되는 상태
    - 준영속 : 영속성 컨텍스트에 저장되었다가 분리된 상태
    - 삭제(removed) : 삭제된 상태
    
    ** em.persist(member); -> 영속성 컨텍스트에 저장을 했다고 이 시점에 DB 쿼리가 날라가지 않는다.
      -* 트랜잭션 커밋하는 시점에 - tx.commit(); 영속성 컨텍스트에 있는 데이터가 DB로 가서 그 시점에 쿼리가 날라감
    
    * 영속성 컨텍스트의 이점
    - 1차 캐시
    - 동일성(Identity) 보장
    - 트랜잭션을 지원하는 쓰기 지연
    - 변경 감지(Dirty Cheking)
    - 지연 로딩(Lazy Loading)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -24,16 +24,30 @@ public class JpaMain {
 //            Member findMember = em.find(Member.class, 1L); // 조회
 //            findMember.setName("HelloJPA"); // 수정
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class) // JPQL - member 테이블이 아닌 객체를 대상으로 쿼리를 짠다.
-                    .setFirstResult(0) // 페이지네이션 0~10 가져와
-                    .setMaxResults(10)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class) // JPQL - member 테이블이 아닌 객체를 대상으로 쿼리를 짠다.
+//                    .setFirstResult(0) // 페이지네이션 0~10 가져와
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.getName() = " + member.getName());
+//            }
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            //비영속 - 객체를 생성한 상태
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            tx.commit();
+            //영속 - 객체를 저장한 상태
+            System.out.println("===BEFORE===");
+            em.persist(member);
+
+//            em.detach(member); // 회원 엔티티를 영속성 컨텍스트에서 분리 - 준영속 상태
+//            em.remove(member); // 실제 DB 데이터 삭제 - 객체를 삭제한 상태
+
+            System.out.println("===AFTER===");
+
+            tx.commit(); // 트랜잭션 커밋하는 시점에 영속성 컨텍스트에 있는 데이터가 DB로 -> 이 시점에 쿼리가 날라감
         } catch (Exception e) {
             tx.rollback();
         } finally {

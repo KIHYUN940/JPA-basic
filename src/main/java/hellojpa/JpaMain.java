4a1e3070adae599b4ab387f178af38f864e9602d
kihyun940 <houng027@gmail.com>
2024-02-07 21:04:45 +0900

    Feat: 영속성 컨텍스트 개념 파악2
    * 영속성 컨텍스트의 이점
    - 1차 캐시
    - 동일성(Identity) 보장
    - 트랜잭션을 지원하는 쓰기 지연
    - 변경 감지(Dirty Cheking)
    - 지연 로딩(Lazy Loading)
    
    - 1차 캐시
      - 영속성 컨텍스트 안에는 1차 캐시가 내부적으로 존재하는데 find("member1")로 조회 시 1차 캐시에 들어있지 않으면 DB에서 가져와서 1차 캐시에 저장하고 다음에 같은 걸 조회 시 1차 캐시에서 가져온다. 단. 영속성 컨텍스트가 종료되면 1차 캐시도 다 사라지는데 사실상 크게 최적화에 도움은 되지 않는다.
    
    - 영속 엔티티의 동일성 보장
      - 자바 컬렉션을 사용해 저장한걸 조회해서 비교 시 같은 엔티티 동일성이 보장되는거 처럼 JPA도 마찬가지다. - 1차 캐시 조회로 인해 동일성 보장
      - (findMember1 == findMember2)); //true - 영속 엔티티의 동일성 보장 - 같은 트랜잭션 안에서 실행 시
    
    - 트랜잭션을 지원하는 쓰기 지연
      - em.persist(member1);
        em.persist(member2); --> 바로 쿼리를 날리는게 아니라, 커밋 전까지 "쓰기 지연 SQL 저장소"에서 쿼리가 쌓임
      - 버퍼링을 모아서 write 하는 이점을 얻을 수 있음
    
    - 변경 감지
      - 영속성 컨텍스트가 만들어지고 1차 캐시가 생성될 때 @Id, Entity가 만들어지면서 동시에 스냅샷도 만들어지는데 이 스냅샷과 비교해서 변경 감지를 해 굳이 em.persist(memberA);를 쓰지 않아도 자동으로 내부적으로 쓰기 지연 SQL 저장소에 update 쿼리를 쌓아두고 커밋 시 쿼리를 날린다. -> member.setName("ZZZZZ");
      - 엔티티 삭제 em.remove(memberA) 또한 변경 감지 메커니즘으로 인해 엔티티를 삭제할 수 있다.
    
    ===
    
    - Member
      - public Member(){} - 생성자 추가
        - JPA는 리플렉션을 사용하여 엔티티 객체를 생성하는데 이때 기본 생성자가 없으면 객체를 생성할 수 없다.
    
    - persistence.xml 추가
      - <property name="hibernate.jdbc.batch_size" value="10"/> 추가
        - size만큼 모아서 데이터베이스에 네트워크로 쿼리를 다 보내고 커밋 - 버퍼링 같은 개념

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -34,18 +34,36 @@ public class JpaMain {
 //            }
 
             //비영속 - 객체를 생성한 상태
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
-            //영속 - 객체를 저장한 상태
-            System.out.println("===BEFORE===");
-            em.persist(member);
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            //영속 - 객체를 저장한 상태
+//            System.out.println("===BEFORE===");
+//            em.persist(member);
 
 //            em.detach(member); // 회원 엔티티를 영속성 컨텍스트에서 분리 - 준영속 상태
 //            em.remove(member); // 실제 DB 데이터 삭제 - 객체를 삭제한 상태
 
-            System.out.println("===AFTER===");
+//            System.out.println("===AFTER===");
+
+//            Member findMember1 = em.find(Member.class, 101L); // 영속성 컨텍스트 초기화 상태에서 1차 캐시에 가져올게 없을 때 DB에서 가져온다. -> 쿼리로 가져옴
+//            Member findMember2 = em.find(Member.class, 101L); // 1차 캐시에서 조회 -> DB에서 조회 X
+//
+//            System.out.println("result = " + (findMember1 == findMember2)); //true - 영속 엔티티의 동일성 보장 - 같은 트랜잭션 안에서 실행 시
+
+            //영속
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2); // --> 커밋 전까지 쓰기 지연 SQL 저장소에서 쿼리가 쌓임 / 버퍼링을 모아서 write 하는 이점을 얻을 수 있음
+
+            //변경 감지
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ"); // 현재 DB에 150 : A값을 ZZZZZ로 변경 -> em.persist(member);를 쓸 필요가 없다. - 변경 감지
+
+            System.out.println("==============");
 
             tx.commit(); // 트랜잭션 커밋하는 시점에 영속성 컨텍스트에 있는 데이터가 DB로 -> 이 시점에 쿼리가 날라감
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -15,6 +15,13 @@ public class Member {
 //    @Column(name = "username") // 컬럼 네임
     private String name;
 
+    public Member(){} // JPA는 리플렉션을 사용하여 엔티티 객체를 생성하는데 이때 기본 생성자가 없으면 객체를 생성할 수 없다.
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }

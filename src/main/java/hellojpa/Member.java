2f902b7a7f384fdd674863c33ce756f6427b7f66
kihyun940 <houng027@gmail.com>
2024-02-08 23:54:18 +0900

    Feat: 객체와 테이블 매핑 * @Entity - @Entity가 붙은 클래스는 JPA가 관리 - JPA를 사용해서 테이블과 매핑할 클래스는 @Entity 필수
    주의점
    - 기본 생성자 필수(파라미터가 없는 public 또는 protected 생성자)
      - JPA는 리플렉션을 사용하여 엔티티 객체를 생성하는데 이때 기본 생성자가 없으면 객체를 생성할 수 없다.
      - 참고 : https://www.inflearn.com/questions/105043/%EA%B8%B0%EB%B3%B8-%EC%83%9D%EC%84%B1%EC%9E%90%EC%97%90-%EA%B4%80%ED%95%B4-%EC%A7%88%EB%AC%B8%EB%93%9C%EB%A6%BD%EB%8B%88%EB%8B%A4
    
    - final 클래스, enum, interface, inner 클래스 사용 X
    - 저장할 필드에 final 사용 X
    
    * @Table
    - @Table은 엔티티와 매핑할 테이블 지정
    - @Table(name = "MBR") // 매핑할 테이블 이름이 MBR일 경우 사용
    - 그 외에도 catalog, schema, uniqueConstraints(DDL) 속성이 있다.

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,8 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-//@Table(name = "USER") // 테이블 네임
+@Entity // JPA를 사용해서 테이블과 매핑할 클래스는 @Entity 필수
+@Table(name = "MBR") // 매핑할 테이블이 MBR일 경우 사용
 public class Member {
 
     @Id
@@ -15,7 +15,7 @@ public class Member {
 //    @Column(name = "username") // 컬럼 네임
     private String name;
 
-    public Member(){} // JPA는 리플렉션을 사용하여 엔티티 객체를 생성하는데 이때 기본 생성자가 없으면 객체를 생성할 수 없다.
+    public Member(){} // JPA는 리플렉션을 사용하여 엔티티 객체를 생성하는데 이때 기본 생성자가 없으면 객체를 생성할 수 없다. / 기본 생성자 필수
 
     public Member(Long id, String name) {
         this.id = id;

33e639528733e0b4754cfc7f5f4e27d4fac095e5
kihyun940 <houng027@gmail.com>
2024-02-09 21:38:35 +0900

    Feat: 데이터베이스 스키마 자동 생성
    - DDL을 애플리케이션 실행 시점에 자동 생성
    - 데이터베이스 방언을 활용해서 데이터베이스에 맞는 적절한 DDL 생성
    - 생성된 DDL은 운영서버에서는 사용하지 않거나 적절히 다듬은 후 사용
    
    데이터베이스 스키마 자동 생성 - 속성
    - persistence.xml
      - hibernate.hbm2ddl.auto 추가
        - value = "create" -> 기존 테이블 삭제 후 다시 생성(DROP + CREATE)
        - "create-drop" -> create와 같으나 종료 시점에 테이블을 DROP
        - "update" -> 변경 부분만 반영 - 단, 운영DB에는 사용하면 안됨
        - "validate" -> 엔티티와 테이블이 정상 매핑되었는지만 확인
        - "none" -> 사용하지 않음
    
        *** 주의
        - 운영 장비에는 절대 create, create-drop, update를 사용하면 안된다.
        - 개발 초기 단계는 create, update
        - 테스트 서버는 update, validate 사용하는데 웬만해선 update도 쓰지 말자
        - 스테이징과 운영서버는 vaildate 또는 none
    
    DDL 생성 기능
     - 제약조건 추가
       - @Column(nullable = false, length = 10)
     - 유니크 제약조건 추가
       - @Column(unique = true, length = 10)
    
     - @Table(name = "MBR") 같은 이름 속성을 바꿀 땐 런타임에 영향을 끼치지만, @Column의 제약조건은 JPA 런타임 동작에는 영향을 미치지 않는다.
    
       ** DDL 생성 기능은 DDL을 자동 생성할 때만 사용되고 JPA의 실행 로직에는 영향을 주지 않는다. -> 런타임에 영향을 끼치지 않는다.

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,13 +6,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity // JPA를 사용해서 테이블과 매핑할 클래스는 @Entity 필수
-@Table(name = "MBR") // 매핑할 테이블이 MBR일 경우 사용
+//@Table(name = "MBR") // 매핑할 테이블이 MBR일 경우 사용
 public class Member {
 
     @Id
     private Long id;
 
 //    @Column(name = "username") // 컬럼 네임
+    @Column(unique = true, length = 10)
     private String name;
 
     public Member(){} // JPA는 리플렉션을 사용하여 엔티티 객체를 생성하는데 이때 기본 생성자가 없으면 객체를 생성할 수 없다. / 기본 생성자 필수

c597f94fb0d96a7ebd5e6753e2c82d776fb3dee5
kihyun940 <houng027@gmail.com>
2024-02-12 19:02:57 +0900

    Feat: 연관관계 매핑 / 객체 지향 모델링 - 단방향 연관관계
    객체 지향 모델링 - 단방향
    - 객체의 참조와 테이블의 외래 키를 매핑
      - @ManyToOne(fetch = FetchType.LAZY) // member - N, Team - 1 N:1 관계
            @JoinColumn(name = "TEAM_ID") // 테이블의 외래키를 매핑
            private Team team;
    
    - 참조로 연관관계 조회 - 객체 그래프 탐색

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,21 +15,45 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //저장
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
+            Team team2 = new Team();
+            team2.setName("TeamB");
+            em.persist(team2);
+
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeamId(team.getId());
+
+            member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
+//            member.setTeam(team2); // 연관관계 수정
+//            em.persist(member);
+
+            //조회
             Member findMember = em.find(Member.class, member.getId());
 
-            Long findTeamId = findMember.getTeamId();
-            Team findTeam = em.find(Team.class, findTeamId);
+            Team findTeam = findMember.getTeam();
 
+            System.out.println("findTeam = " + findTeam.getName());
 
+            //다른 팀으로 바꾸고 싶을 때
+            Team newTeam = em.find(Team.class, 2L); // DB에 2번 팀이 있다고 가정
+            System.out.println("newTeam = " + newTeam.getName());
+            findMember.setTeam(newTeam);
+
+            System.out.println("findTeam = " + findTeam.getId());
+            /*
+            정리하자면 여기서 findTeam에 할당된 값은 TeamA라 findMember.setTeam(newTeam)을 호출한 후에도 findTeam 값은 TeamA인데,
+            속성값은 변경되기 때문에 DB상에서는 member1과 TeamB가 매핑
+            * findMember 객체의 속성은 변경되어 DB 상에서는 "member1"과 "TeamB"가 매핑되는 것
+             */
 
             System.out.println("==========");
 

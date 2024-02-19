88746652c1c3a8937f0e653bcaf8b8087b1bd8ab
kihyun940 <houng027@gmail.com>
2024-02-20 01:49:27 +0900

    Feat: 상속관계 매핑 - 싱글 테이블 전략 단일 테이블 전략(싱글 테이블) -> 상속 구조를 가진 객체들을 모두 한 개의 테이블에 저장하는 방식 -> 조인 전략을 사용하다 성능이 저하되는 현상 때문에 DBA와 조율 후 싱글 테이블 전략으로 바꿀 수 있다. -> 조인을 하지 않고 Insert 쿼리도 2번이 아닌 1번만 발생되기에 성능면에서 효율적이다.
    - @Inheritance(strategy = InheritanceType.SINGLE_TABLE) 수정
      - JPA의 큰 장점 중 하나 - 큰 코드를 바꾸지 않고 InheritanceType만 바꿔줬는데도 데이터베이스 전략을 바꿀 수 있다.
    
    - @DiscriminatorColumn
      - 조인 전략에선 이 애너테이션이 없으면 안 생겼는데  싱글 테이블 전략에선 이 애너테이션이 없어도 DTYPE이 생긴다. / 결론은 DTYPE이 있는게 운영상 좋다.
    
    장점
    • 조인이 필요 없으므로 일반적으로 조회 성능이 빠름
    • 조회 쿼리가 단순함
    
    단점
    • 자식 엔티티가 매핑한 컬럼은 모두 null 허용
    • 단일 테이블에 모든 것을 저장하므로 테이블이 커질 수 있다. 상황에 따라서 조회 성능이 오히려 느려질 수 있다.

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,8 +3,8 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)  // JPA의 큰 장점 중 하나 - 큰 코드를 바꾸지 않고 InheritanceType만 바꿔줬는데도 데이터베이스 전략을 바꿀 수 있다.
+@DiscriminatorColumn // 조인 전략에선 이 애너테이션이 없으면 안 생겼는데  싱글 테이블 전략에선 이 애너테이션이 없어도 DTYPE이 생긴다. / 결론은 DTYPE이 있는게 운영상 좋다.
 public class Item {
 
     @Id @GeneratedValue

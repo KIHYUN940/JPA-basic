af2c0327e8a0ea363b9dff242b02572f8eddb695
kihyun940 <houng027@gmail.com>
2024-02-22 00:09:22 +0900

    Feat: 프록시 개념 및 활용 Member와 Team을 가져오는 로직에선 연관관계 매핑으로 인해 둘다 가져와도 상관 없지만 member만 가져오려고 할 때 team이 매핑되어 있기에 team까지 가져오면 성능상 손해 -> JPA는 지연로딩, 프록시 등으로 해결
    em.find(): 데이터베이스를 통해서 실제 엔티티 객체 조회
    em.getReference(): 데이터베이스 조회를 미루는 가짜(프록시) 엔티티 객체 조회
    
    프록시 특징
    - 실제 클래스를 상속 받아서 만들어짐
    - 실제 클래스와 겉 모양이 같다.
    - 사용하는 입장에서는 진짜 객체인지 프록시 객체인지 구분하지 않고 사용하면 됨
    
    - 프록시 객체는 실제 객체의 참조(target)를 보관
    - 프록시 객체를 호출하면 프록시 객체는 실제 객체의 메소드
    
    프록시 객체의 초기화
    Member member1 = em.getReference(Member.class, member1.getId()); //getReference()를 호출하는 시점엔 DB에 쿼리가 날라가지 않음
    member1.getName(); //member1의 값을 실제 사용되는 시점엔 DB에 쿼리가 날라감
    
    특징
    - 프록시 객체는 처음 사용할 때 한 번만 초기화
    
    - 프록시 객체를 초기화 할 때, 프록시 객체가 실제 엔티티로 바뀌는 것은 아님, 초기화되면 프록시 객체를 통해서 실제 엔티티에 접근 가능
    
    - 프록시 객체는 원본 엔티티를 상속받음, 따라서 타입 체크시 주의해야함 (== 비교 실패, 대신 instance of 사용)
    
    - 영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해도 실제 엔티티 반환
    
    - 영속성 컨텍스트의 도움을 받을 수 없는 준영속 상태일 때, 프록시를 초기화하면 문제 발생
    -> 하이버네이트는 org.hibernate.LazyInitializationException 예외를 터트림
    
    ** JPA - 같은 영속성 컨텍스트 안에서 조회를 하면 항상 같은 값이 나온다. - true
    
    프록시 확인
    - emf.getPersistenceUnitUtil().isLoaded(refMember)); //프록시 인스턴스의 초기화 여부 확인
    - Hibernate.initialize(refMember); //프록시 강제 초기화
    - JPA 표준은 강제 초기화 없음 -> 강제 호출 : ex) member.getUsername()

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,26 +18,96 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreateBy("kim");
-            member.setCreateDate(LocalDateTime.now());
+//            Member member = em.find(Member.class, 1L);
+//
+//            printMember(member); // 아래처럼 Member와 Team을 가져오는 로직에선 연관관계 매핑으로 인해 둘다 가져와도 상관 없지만 지금처럼 member만 가져오려고 할 때 team이 매핑되어 있기에 team까지 가져오면 성능상 손해 -> JPA는 지연로딩, 프록시 등으로 해결
+////            printMemberAndTeam(member);
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
-            //영속성 컨텍스트에 있는 걸 DB에 다 날리고 영속성 컨텍스트를 비움
+//            Member member2 = new Member();
+//            member2.setUsername("member2");
+//            em.persist(member2);
+
             em.flush();
             em.clear();
+
+//            Member findMember = em.find(Member.class, member.getId());
+
+//            Member findMember = em.getReference(Member.class, member1.getId()); //getReference()를 호출하는 시점엔 DB에 쿼리가 날라가지 않음 / findMember의 값을 실제 사용되는 시점엔 DB에 쿼리가 날라감
+//            System.out.println("findMember = " + findMember.getClass()); // -> findMember = class hellojpa.Member$HibernateProxy$WO0oD6s5 - 프록시 클래스, 가짜 클래스
+//            System.out.println("findMember.id = " + findMember.getId()); // 이미 getReference()로 찾을 때 id값을 찾아줬기에 이 부분은 쿼리가 안 날라감
+//            System.out.println("findMember.username = " + findMember.getUsername());
+//            System.out.println("findMember.username = " + findMember.getUsername()); // 위에서 한번 프록시 객체가 영속성 컨텍스트에 초기화 요청을 해서 DB가 실제 엔티티를 생성했기에 더이상 요청을 하지 않고 바로 DB에 접근해서 데이터를 가져온다.
+
+
+//            Member refMember = em.getReference(Member.class, member1.getId()); // 영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해도 실제 엔티티 반환 - hellojpa.Member - 프록시로 반환해도 실제 얻는 이점이 없기 때문
+//            System.out.println("refMember = " + refMember.getClass());
+//
+//            Member findMember = em.find(Member.class, member1.getId());
+//            System.out.println("findMember = " + findMember.getClass());
+
+            /*
+            JPA - 같은 영속성 컨텍스트 안에서 조회를 하면 항상 같은 값이 나온다. - true
+            그렇기 때문에 위에 refMember = proxy에서 받아오고 refMember == findMember = true가 성립되어야 하기에 findMember도 proxy에서 받아온다.
+            핵심은 JPA가 proxy에서 조회하던 DB에서 조회하던 맞춰준다.
+             */
+//            System.out.println("refMember == findMember : " + (refMember == findMember)); //true
+
+//            Member m2 = em.getReference(Member.class, member2.getId());
+//
+//            logic(m1, m2); // ctrl + alt + m - 메서드로 추출
+
+
+            //준영속 상태일 때 프록시 초기화 문제 발생
+//            Member refMember = em.getReference(Member.class, member1.getId());
+//            System.out.println("refMember = " + refMember.getClass());
+//
+//            em.detach(refMember); //영속성 컨텍스트에서 빼버림 / 영속성 컨텍스트가 더이상 관리 X
+////            em.close(); //영속성 컨텍스트 close -> 5.4.1.Final 버전부터 예외가 발생하지 않음
+////            em.clear();
+//
+//            refMember.getUsername();
+
+
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass());
+
+//            refMember.getUsername(); //프록시 초기화
+            Hibernate.initialize(refMember); //강제 초기화
+            System.out.println("emf.getPersistenceUnitUtil().isLoaded(refMember) = " + emf.getPersistenceUnitUtil().isLoaded(refMember)); //프록시 인스턴스의 초기화 여부 확인
+
 
             System.out.println("=======");
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
 
         emf.close();
     }
+
+//    private static void logic(Member m1, Member m2) {
+//        System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); //false - m1, m2에 실제 객체가 들어올지 프록시로 들어올지 모르기 때문에 타입 비교는 절대 ==으로 하지 말고 instanceof 사용
+//        System.out.println("m1 instanceof m2 : " + (m1 instanceof Member)); //true - 타입 비교는 꼭 instanceof 사용
+//        System.out.println("m1 instanceof m2 : " + (m2 instanceof Member));
+//    }
+
+//    private static void printMember(Member member) {
+//        System.out.println("member = " + member.getUsername());
+//    }
+//
+//    private static void printMemberAndTeam(Member member) {
+//        String username = member.getUsername();
+//        System.out.println("username = " + username);
+//
+//        Team team = member.getTeam();
+//        System.out.println("team = " + team.getName());
+//    }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -23,16 +23,16 @@ public class Member extends BaseEntity {
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false) // 일대다에서 양방향 적용 - insert, upate를 false로 적용해서 결과적으로 읽기 전용으로 만듦 - 이런 매핑은 공식적으로 존재 X - 실무에서 복잡하게 하다보면 쓸 일이 가끔 생기기도 함 -> * 결론은 일대다 단방향, 양방향 쓰지말고 다대일 양방향으로 사용!!
     private Team team;
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
+//    @OneToOne
+//    @JoinColumn(name = "LOCKER_ID")
+//    private Locker locker;
 
 //    @ManyToMany
 //    @JoinTable(name = "MEMBER_PRODUCT")
 //    private List<Product> products = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
+//    @OneToMany(mappedBy = "member")
+//    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -48,5 +48,13 @@ public class Member extends BaseEntity {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }

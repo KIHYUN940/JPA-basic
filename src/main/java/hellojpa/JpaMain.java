55254f05b7c87f0b671e1ae9854b254ac55c86de
kihyun940 <houng027@gmail.com>
2024-02-23 05:09:42 +0900

    Feat: 영속성 전이(CASCADE)와 고아 객체 영속성 전이 - CASCADE - 특정 엔티티를 영속 상태로 만들 때 연관된 엔티티도 함께 영속 상태로 만들도 싶을 때   - 부모 엔티티를 저장할 때 자식 엔티티도 함께 저장 - @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)   private List<Child> childList = new ArrayList<>();
    CASCADE - 주의
    - 영속성 전이는 연관관계를 매핑하는 것과 아무 관련이 없음
    - 엔티티를 영속화할 때 연관된 엔티티도 함께 영속화하는 편리함을 제공할 뿐이다.
    
    CASCADE의 종류
    • ALL: 모두 적용
    • PERSIST: 영속
    • REMOVE: 삭제
    • MERGE: 병합
    • REFRESH: REFRESH
    • DETACH: DETACH
    - 보통 CascadeType.ALL or PERSIST 사용
    
    고아 객체
    - 고아 객체 제거: 부모 엔티티와 연관관계가 끊어진 자식 엔티티를 자동으로 삭제
    - orphanRemoval = true 사용
    
    @OneToMany(mappedBy = "parent", orphanRemoval = true)
    private List<Child> childList = new ArrayList<>();
    
    Parent parent1 = em.find(Parent.class, id);
    parent1.getChildren().remove(0);
    //자식 엔티티를 컬렉션에서 제거하면 자동으로 고아 객체도 제거
    
    고아 객체 - 주의
    - 참조하는 곳이 하나일 때 사용해야함
    - 특정 엔티티가 개인 소유할 때 사용
    - @OneToOne, @OneToMany만 가능
    
    ** 개념적으로 부모를 제거하면 자식은 고아가 된다. 따라서 고아 객체 제거 기능을 활성화 하면 부모를 제거할 때 자식도 함께 제거된다. 이것은 CascadeType.REMOVE처럼 동작한다.
    
    영속성 전이 + 고아 객체, 생명주기
    - CascadeType.ALL + orphanRemoval=true 함께 사용 가능
    - 두 옵션을 모두 활성화 하면 부모 엔티티를 통해서 자식의 생명주기를 관리할 수 있음
    - 도메인 주도 설계(DDD)의 Aggregate Root개념을 구현할 때 유용

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,53 +18,21 @@ public class JpaMain {
 
         try {
 
-//            Member member = em.find(Member.class, 1L);
-//
-//            printMember(member); // 아래처럼 Member와 Team을 가져오는 로직에선 연관관계 매핑으로 인해 둘다 가져와도 상관 없지만 지금처럼 member만 가져오려고 할 때 team이 매핑되어 있기에 team까지 가져오면 성능상 손해 -> JPA는 지연로딩, 프록시 등으로 해결
-////            printMemberAndTeam(member);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
+            em.persist(parent);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
-            //영속성 컨텍스트에 있는 데이터들을 DB에 옮기고 영속성 컨텍스트는 비워두기 위해서 사용
             em.flush();
             em.clear();
 
-//            Member m = em.find(Member.class, member1.getId());
-
-//            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass()); //Team - 지연 로딩 LAZY 설정해뒀기에 프록시로 조회 / EAGER로 설정하면 즉시 로딩 - 프록시가 아닌 진짜 객체로 조회
-//
-//            System.out.println("====================");
-//            m.getTeam().getName(); //LAZY - 프록시 - 이 부분에서 쿼리가 날라감 - 실제 team을 사용하는 시점에(가져올 때가 아님) 초기화(DB조회) /// EAGER - 즉시 로딩 - 위에서 이미 조인했기에 쿼리가 날라가지 않음
-//            System.out.println("====================");
-
-//            List<Member> members = em.createQuery("select m from Member m", Member.class) // EAGER 즉시로딩 - JPQL 사용 시 N+1 문제 일으킬 수 있음 / em.find()는 JPA 내부적으로 다 찾아서 쿼리 날리기에 N+1 문제 발생 X
-//                    .getResultList(); //LAZY 지연로딩으로 바꾸면 N+1 문제 해결 -> LAZY로 바꿨기 때문에 그 객체를 프록시 객체로 감싸져 따로 따로 로딩이 된다.
-
-
-            //fetch join - LAZY를 사용하더라도 JPQL에서 fetch join을 하면 조인 가능 - 대부분 이 방법으로 해결 - 엔티티 그래프 기능 및 배치로 해결하는 방법도 있음
-            List<Member> members2 = em.createQuery("select m from Member m join fetch m.team", Member.class)
-                    .getResultList();
-
-            /*
-            N+1 문제
-            - 첫 쿼리 1개를 날렸는데 그것 때문에 추가적인 N개의 쿼리가 더 나가서 문제 발생되는 것
-             */
+            Parent findParent = em.find(Parent.class, parent.getId());
+//            findParent.getChildList().remove(0);
+            em.remove(findParent);
 
 
             System.out.println("=======");

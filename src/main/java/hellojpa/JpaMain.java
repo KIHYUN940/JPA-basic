db2ce3e140f6f93c61e9f1004538d3a364ccc094
kihyun940 <houng027@gmail.com>
2024-02-25 04:59:48 +0900

    Feat: 값 타입 - 임베디드 타입 * 임베디드 타입 - 새로운 값 타입을 직접 정의할 수 있음 - JPA는 임베디드 타입(embedded type)이라 함 - 주로 기본 값 타입을 모아서 만들어서 복합 값 타입이라고도 함 - int, String과 같은 값 타입
    - @Embeddable: 값 타입을 정의하는 곳에 표시
    - @Embedded: 값 타입을 사용하는 곳에 표시
    - 기본 생성자 필수 *
    
    임베디드 타입 설정
    Address - 주소, Period - 기간
    
    임베디드 타입의 장점
    - 재사용성
    - 높은 응집도가 있다.
    - Period.isWork()처럼 해당 값 타입만 사용하는 의미 있는 메소드를 만들 수 있음
    - 임베디드 타입을 포함한 모든 값 타입은 값 타입을 소유한 엔티티에 생명주기를 의존함
    
    임베디드 타입과 테이블 매핑
    - 임베디드 타입은 엔티티의 값일 뿐이다.
    - ** 임베디드 타입을 사용하기 전과 후에 매핑하는 테이블은 같다.
    - 객체와 테이블을 아주 세밀하게(find-grained) 매핑하는 것이 가능
    - 잘 설계한 ORM 애플리케이션은 매핑한 테이블의 수보다 클래스의 수가 더 많음
    
    한 엔티티에서 값은 값 임베디드 타입 사용 시
    - @AttributeOverrides, @AttributeOverride를 사용해서 컬럼명 속성을 재정의 한다.
    
    @Embedded
        @AttributeOverrides({
                @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
                @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
                @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
        })
        private Address workAddress;

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,22 +18,12 @@ public class JpaMain {
 
         try {
 
-            Child child1 = new Child();
-            Child child2 = new Child();
+            Member member = new Member();
+            member.setUsername("hello");
+            member.setHomeAddress(new Address("city", "street", "10000"));
+            member.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
 
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
-
-            em.persist(parent);
-
-            em.flush();
-            em.clear();
-
-            Parent findParent = em.find(Parent.class, parent.getId());
-//            findParent.getChildList().remove(0);
-            em.remove(findParent);
-
+            em.persist(member);
 
             System.out.println("=======");
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,7 +9,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity // JPA를 사용해서 테이블과 매핑할 클래스는 @Entity 필수
-public class Member extends BaseEntity {
+public class Member {
 
     @Id
     @GeneratedValue
@@ -19,10 +19,31 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY) //지연 로딩 LAZY를 사용하면 team을 프록시 객체로 조회한다.
-//    @ManyToOne(fetch = FetchType.EAGER) //즉시 로딩 - 예를 들어 Member와 Team을 조회할 때 항상 묶어서 조회할 때 EAGER 사용
-    @JoinColumn(name = "TEAM_ID") // 일대다에서 양방향 적용 - insert, upate를 false로 적용해서 결과적으로 읽기 전용으로 만듦 - 이런 매핑은 공식적으로 존재 X - 실무에서 복잡하게 하다보면 쓸 일이 가끔 생기기도 함 -> * 결론은 일대다 단방향, 양방향 쓰지말고 다대일 양방향으로 사용!!
-    private Team team;
+    //Period - 기간
+    @Embedded
+    private Period workPeriod;
+
+    //Address - 주소
+    @Embedded
+    private Address homeAddress;
+
+    //한 엔티티에서 같은 값 타입 사용 시
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
+
+
+
+
+
+//    @ManyToOne(fetch = FetchType.LAZY) //지연 로딩 LAZY를 사용하면 team을 프록시 객체로 조회한다.
+////    @ManyToOne(fetch = FetchType.EAGER) //즉시 로딩 - 예를 들어 Member와 Team을 조회할 때 항상 묶어서 조회할 때 EAGER 사용
+//    @JoinColumn(name = "TEAM_ID") // 일대다에서 양방향 적용 - insert, upate를 false로 적용해서 결과적으로 읽기 전용으로 만듦 - 이런 매핑은 공식적으로 존재 X - 실무에서 복잡하게 하다보면 쓸 일이 가끔 생기기도 함 -> * 결론은 일대다 단방향, 양방향 쓰지말고 다대일 양방향으로 사용!!
+//    private Team team;
 
 //    @OneToOne
 //    @JoinColumn(name = "LOCKER_ID")
@@ -34,6 +55,7 @@ public class Member extends BaseEntity {
 
 //    @OneToMany(mappedBy = "member")
 //    private List<MemberProduct> memberProducts = new ArrayList<>();
+
 
     public Long getId() {
         return id;
@@ -51,11 +73,19 @@ public class Member extends BaseEntity {
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
 }

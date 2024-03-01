0bbcc65c5f0f3d30d6398d13069aabd899bb9aa1
kihyun940 <houng027@gmail.com>
2024-03-01 23:48:27 +0900

    Feat: 객체 지향 쿼리 언어(JPQL) & QueryDSL JPA는 다양한 쿼리 방법을 지원 • JPQL • JPA Criteria • QueryDSL • 네이티브 SQL • JDBC API 직접 사용, MyBatis, SpringJdbcTemplate 함께 사용
    JPQL
    - 가장 단순한 조회 방법
    - EntitiyManager.find()
    - 객체 그래프 탑색 (a.getB().getC())
    
    - JPA를 사용하면 엔티티 객체를 중심으로 개발
    - 검색을 할 때도 테이블이 아닌 엔티티 객체를 대상으로 검색
    
    -> 애플리케이션이 필요한 데이터만 DB에서 불러오려면 결국 검색 조건이 포함된 SQL 필요
    
    - JPQL은 엔티티 객체를 대상으로 쿼리
    - SQL은 데이터베이스 테이블을 대상으로 쿼리
    
    //JPQL
    List<Member> resultList = em.createQuery("select m from Member m where m.username like '%kim%'", Member.class).getResultList();
    
    - JPQL을 한마디로 정의하면 객체 지향 SQL
    
    Criteria
    
    장점 : 1. 오타 시 컴파일 시점에 컴파일 오류가나서 잡기 쉽다.
          2. 동적 쿼리를 짜기 좋다.
    
    단점 : SQL스럽지가 않다.
    
    JPA 스펙에는 있지만, 실무에서 잘 쓰지 않는다. -> 코드가 복잡해지기 때문에 유지보수가 힘들어짐
    -> Criteria 대신 QueryDSL 사용 권장 - 오픈소스 라이브러리
    
    QueryDSL
    
    - 문자가 아닌 자바코드로 JPQL을 작성할 수 있음
    - JPQL 빌더 역할
    - 컴파일 시점에 문법 오류를 찾을 수 있음
    - 동적 쿼리 작성 편리
    - 단순하고 쉽다
    - JPQL 문법을 알고 사용하면 된다.
    -> 실무 사용 권장
    
    네이티브 SQL
    
    - JPA가 제공하는 SQL을 직접 사용하는 기능
    - JPQL로 해결할 수 없는 특정 데이터베이스에 의존적인 기능
    - ex) 오라클 CONNECT BY, 특정 DB만 사용하는 SQL 힌트
    
    JDBC 직접 사용, SpringJdbcTemplate, MyBatis 등
    
    엔티티매니저 안에서 쿼리를 날리면 자동으로 flush 해줘서 DB에 데이터를 넣고 쿼리로 찾을 수 있는데,
    ex) dbconn.executeQuery("select * from member"); 같이 JDBC 커넥션을 직접 사용하거나, 스프링 JdbcTemplate, 마이바티스 등을 사용하는 경우 강제로 수동 flush를 해줘야 DB에서 쿼리로 조회가 가능하다.
    -> em.flush를 해준 후 쿼리 코드 날려야 함

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,9 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
@@ -19,76 +22,69 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            //값 타입 컬렉션
-//            member.getAddressHistory().add(new Address("old1", "street", "10000"));
-//            member.getAddressHistory().add(new Address("old2", "street", "10000"));
-
-            //값 타입 컬렉션 -> 엔티티 사용
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("=====================");
-
-            // memberId 조회 시 컬렉션 값 타입들은 지연로딩이라 불러오지 않는다.
-            Member findMember = em.find(Member.class, member.getId());
-
-//            List<Address> addressHistory = findMember.getAddressHistory();
-//            System.out.println("---------------");
-//            // ** 컬렉션 값 타입은 디폴트가 지연로딩 전략 사용 - 이 시점에 쿼리 나감
-//            for (Address address : addressHistory) {
-//                System.out.println("address.getCity() = " + address.getCity());
+            //JPQL
+//            List<Member> resultList = em.createQuery("select m from Member m where m.username like '%kim%'", Member.class)
+//                    .getResultList();
+//
+//            for (Member member : resultList) {
+//                System.out.println("member = " + member);
 //            }
 
-            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-            System.out.println("------------------");
-            for (String favoriteFood : favoriteFoods) {
-                System.out.println("favoriteFood = " + favoriteFood);
+            //criteria 사용 준비 - JPQL에서 Criteria는 동적으로 쿼리를 생성하기 위한 방법 중 하나
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            Root<Member> m = query.from(Member.class);
+//            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            CriteriaQuery<Member> cq = query.select(m);
+
+            String username = "Hong";
+            if (username != null) {
+                cq = cq.where(cb.equal(m.get("username"), "kim"));
             }
 
-            System.out.println("=============================");
-
-            //homeCity -> newCity - 값 타입은 불변 객체로 설계해야하기 때문에 밑에처럼 수정을 하면 안된다.
-//            findMember.getHomeAddress().setCity("newCity"); // X
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity",a.getStreet(),a.getZipcode())); //값 타입 수정 시 새로운 인스턴스 생성해서 통으로 갈아 끼워야 함
-
-
-            //값 타입 컬렉션 수정 - 치킨 -> 한식  - 이것도 String이기 때문에 수정하려면 통으로 갈아 끼워야 함
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            System.out.println("======================");
-
-            //값 타입 컬렉션 - 주소 old1 -> new1  // Equals hashcode가 잘 구현되어 있어야 함
+            List<Member> resultList = em.createQuery(cq)
+                    .getResultList();
             /*
-            값 타입 컬렉션을 수정할 때 자바의 equals와 hashcode 메서드가 제대로 구현되어 있어야 하는 이유는 컬렉션 내에서 해당 값의 동등성을 판단하기 위해서
-            컬렉션은 객체의 동등성을 판단하기 위해 equals 메서드를 사용하는데 내부적만으로 객체의 동등성을 판단할 수 없기에 객체의 equals 메서드가 올바르게 구현되어 있어야 함
-            String 클래스엔 내부적으로 이미 equals() 메서드가 구현되어 있기에 따로 구현을 하지 않아도 된다.
+            Criteria
+            장점 : 1. 오타 시 컴파일 시점에 컴파일 오류가나서 잡기 쉽다.
+                  2. 동적 쿼리를 짜기 좋다.
+
+            단점 : SQL스럽지가 않다.
+
+            JPA 스펙에는 있지만, 실무에서 잘 쓰지 않는다. -> 코드가 복잡해지기 때문에 유지보수가 힘들어짐
+            -> Criteria 대신 QueryDSL 사용 권장 - 오픈소스 라이브러리
+
+            QueryDSL
+            - 문자가 아닌 자바코드로 JPQL을 작성할 수 있음
+            - JPQL 빌더 역할
+            - 컴파일 시점에 문법 오류를 찾을 수 있음
+            - 동적 쿼리 작성 편리
+            - 단순하고 쉽다
+            - JPQL 문법을 알고 사용하면 된다.
+            -> 실무 사용 권장
              */
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("new1", "street", "10000"));
-            // 값 타입 컬렉션에 변경 사항이 발생하면 주인 엔티티와 연관된 모든 데이터를 삭제하고 값 타입 컬렉션에 있는 현재 값 모두 다시 저장한다. -> 결론은 사용하면 안된다. -> 값 타입 컬렉션 대신 일대다 관계를 고려
+
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member); //commit을 해야 DB에 데이터가 들어감
+
+            //flush -> commit이나 query 날라갈 때 동작
+
+            //네이티브 SQL
+            List<Member> resultList1 = em.createNativeQuery("select MEMBER_ID, city, street, zipcode2, USERNAME from MEMBER", Member.class)
+                    .getResultList();
 
             /*
-            그럼 값 타입 컬렉션은 언제 쓰는가??
-            값 타입 추적이 필요 없을 정도로 체크 박스 [치킨, 피자] 같은 되게 단순한 값을 넣을 때 사용한다. - & 값이 다 사라져도 상관 없거나 업데이트 칠 필요가 없을 때 사용
-            이게 아닌 이상 값 타입 컬렉션을 사용하지 말고 엔티티를 만들어서 사용하자!
-
-            * 식별자가 필요하고 지속해서 값을 추적, 변경해야 한다면 그것은 값 타입이 아닌 엔티티
+            JDBC 직접 사용, SpringJdbcTemplate, MyBatis 등
+            엔티티매니저 안에서 쿼리를 날리면 자동으로 flush 해줘서 DB에 데이터를 넣고 쿼리로 찾을 수 있는데,
+            ex) dbconn.executeQuery("select * from member"); 같이 JDBC 커넥션을 직접 사용하거나, 스프링 JdbcTemplate, 마이바티스 등을 사용하는 경우
+            강제로 수동 flush를 해줘야 DB에서 쿼리로 조회가 가능하다. -> em.flush를 해준 후 쿼리 코드 날려야 함
              */
+
+            for (Member member1 : resultList1) {
+                System.out.println("member1 = " + member1.getUsername());
+            }
 
             System.out.println("=======");
 
